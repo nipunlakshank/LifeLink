32f11dc4c8aaf9aa2045ba27b017aa0918a0bdaa
kosala541 <109388153+kosala541@users.noreply.github.com>
2023-10-05 15:59:59 +0530

    home

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,9 +1,58 @@
 <?php $this->view('includes/header') ?>
 
 <main>
+    <div class="overflow-hidden py-5">
+        <div class="offset-lg-1 col-lg-10">
+            <div class="row">
+                <div class="col-md-3 col-lg-4">
+                    <div class="border border-1 shadow shadow-sm rounded-3 text-center p-3 position-relative">
+                        <div class="ji-center py-2">
+                            <div class="rounded rounded-circle border border-3 position-relative" style="width: 100px; height: 100px;">
+                                <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" class="img-fluid" alt="">
+                            </div>
+                        </div>
+                        <p class="fw-bold opacity-75">Kosala Chathuranga <i class="fa-regular fa-pen-to-square col-2 pointer" onclick="name_edit1();"></i></p>
+                        <div id="name_edit" class="d-none position-absolute border border-1 rounded-3 bg-white col-12 p-3" style="z-index: 9; left: 0;">
+                            <input type="text" value="Kosala" class="form-control mb-2" placeholder="First name">
+                            <input type="text" value="Chathuranga" class="form-control mb-3" placeholder="Last name">
+                            <button class="btn btn-primary btn-sm">Update</button>
+                            <button class="btn btn-danger btn-sm" onclick="name_edit1()">Cancel</button>
+                        </div>
+                        <div class="position-relative text-start" style="font-size: 13px;">
+                            <div class="disc m-0" id="see_all">
+                                <p class="opacity-75 m-0">Excited to share that I've completed the intensive 5-day Swimming and Water Survival Training Course conducted by the Sri Lanka Police. This course delved beyond swimming techniques, focusing on the psychology and physica</p>
+                            </div>
+                            <span class="see_more m-0" onclick="see_more();" id="show_more">Read more</span>
+                            <span class="see_more m-0 d-none" onclick="see_more();" id="show_less">Show less</span>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col d-flex justify-content-between align-items-center">
+                                <input id="mobile" type="text" class="col-10 opacity-75" readonly value="0757033713" style="font-size: 14px; border: none;">
+                                <i class="fa-regular fa-pen-to-square col-2 pointer" onclick="open_edit1();" id="edit_icn1"></i>
+                                <button class="btn btn-primary btn-sm ms-1 d-none" style="font-size: 13px;" id="update_icon1">update</button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col d-flex justify-content-between align-items-center">
+                                <input id="mobile" type="text" class="col-10 opacity-75" readonly value="chathurangakosala65@gmail.com" style="font-size: 14px; border: none;">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col d-flex justify-content-between align-items-center">
+                                <input id="mobile" type="text" class="col-10 opacity-75" readonly value="@kosala541" style="font-size: 14px; border: none;">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9 col-lg-8">
+                    <div class="border border-1 shadow shadow-sm rounded-3">
 
-    <div class="container">
-        <h1>sdlslk</h1>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
 
 </main>
--- app/views/includes/header.view.php
+++ app/views/includes/header.view.php
@@ -28,23 +28,45 @@
 
 <body>
     <header>
-        <div class="" style="background-color: #fff;">
-            <div class="p-3 d-flex">
-                <div class="pe-3">
-                    <img style="width: 40px;" src="<?= APP_LOGO ?>" alt="LOGO" class="">
-                </div>
-                <div class="d-flex justify-content-center align-items-center border border-1 rounded rounded-2">
-                    <i class="fa-solid fa-magnifying-glass mx-2 opacity-75"></i>
-                    <input type="text" style="outline: none; border: none;" placeholder="Search..">
-                </div>
-                <div>
-                    <i class="fa-solid fa-house"></i>
-                </div>
-                <div>
-
-                </div>
-                <div>
-
+        <div class="overflow-hidden" style="background-color: #F4F2EE;">
+            <div class="offset-lg-1 col-lg-10">
+                <div class="d-flex justify-content-between p-2">
+                    <div class="ji-center">
+                        <div class="pe-3 pointer">
+                            <img style="width: 40px;" src="<?= APP_LOGO ?>" alt="LOGO" class="">
+                        </div>
+                        <div class="position-relative p-1 d-flex justify-content-center align-items-center border border-1 rounded rounded-2">
+                            <i class="fa-solid fa-magnifying-glass mx-2 opacity-75 flex_lg"></i>
+                            <i class="fa-solid fa-magnifying-glass mx-2 opacity-75 none_lg" onclick="open_search();"></i>
+                            <input type="text" class="flex_lg" style="outline: none; border: none; background-color: #00000000;" placeholder="Search..">
+                            <div id="mob_search" class="position-absolute d-none" style="z-index: 9; left: -2px;">
+                                <div class="p-1 bg-light d-flex justify-content-center align-items-center border border-1 rounded rounded-2">
+                                    <i class="fa-solid fa-magnifying-glass mx-2 opacity-75"></i>
+                                    <input type="text" class="" style="outline: none; border: none; background-color: #00000000;" placeholder="Search..">
+                                    <i class="fa-solid fa-circle-xmark" onclick="open_search()"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ji-center">
+                        <div class="mx-3 ms-5 text-center border-bottom border-2 border-dark pointer">
+                            <i class="fa-solid fa-house opacity-50 fs-5"></i>
+                            <p class="m-0 flex_lg" style="font-size: 12px;">Home</p>
+                        </div>
+                        <div class="mx-3 text-center border-2 border-dark pointer">
+                            <i class="fa-solid fa-bell opacity-50 fs-5"></i>
+                            <p class="m-0 flex_lg" style="font-size: 12px;">Notification</p>
+                        </div>
+                        <div class="mx-3 text-center border-2 border-dark pointer">
+                            <i class="fa-solid fa-envelope  opacity-50 fs-5"></i>
+                            <p class="m-0 flex_lg" style="font-size: 12px;">Messaging</p>
+                        </div>
+                        <div class="ji-center border-start ps-3 pointer">
+                            <div class="ji-center position-relative rounded rounded-circle" style="width: 35px; height: 35px;">
+                                <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
