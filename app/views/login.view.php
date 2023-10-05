b6e07cf07a1286c25dd56990084389cc1834b5e3
Chandupa <chandupajayalath33@gmail.com>
2023-10-05 17:46:15 +0530

    done

--- app/views/login.view.php
+++ app/views/login.view.php
@@ -7,18 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSS -->
+    <link rel="icon" href="<?= PUBLIC_ROOT ?>/assets/images/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?= PUBLIC_ROOT ?>/assets/css/login.css">
-
+    
     <!-- JS -->
     <script defer src="<?= PUBLIC_ROOT ?>/assets/js/login.js"></script>
 
-
-
-    <title>Login | Gift Zone</title>
+    <title>Login | Lifelink</title>
 
 </head>
 
